ff8f8849c50e2117b4fd3d525a4b2fd41c1e4fcd
Akinori MUSHA <knu@idaemons.org>
2013-11-14 12:41:07 +0900

    v1.0.8

--- skip_2ch_link_pages.user.js
+++ skip_2ch_link_pages.user.js
@@ -36,7 +36,7 @@
 // @license     2-clause BSDL
 // @homepage    https://userscripts.org/scripts/show/178704
 // @homepage    https://github.com/knu/userjs-skip_2ch_link_pages
-// @version     1.0.7
+// @version     1.0.8
 // ==/UserScript==
 
 (function () {
