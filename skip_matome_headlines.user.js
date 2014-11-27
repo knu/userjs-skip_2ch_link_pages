efd270ed15de4effdb1f54fe9a585616d98255a0
Akinori MUSHA <knu@idaemons.org>
2014-11-27 21:40:54 +0900

    v1.0.17

--- skip_matome_headlines.user.js
+++ skip_matome_headlines.user.js
@@ -54,7 +54,7 @@
 // @license     2-clause BSDL
 // @homepage    https://greasyfork.org/scripts/2795-skip-matome-headlines
 // @homepage    https://github.com/knu/userjs-skip_matome_headlines
-// @version     1.0.16
+// @version     1.0.17
 // ==/UserScript==
 
 (function () {
